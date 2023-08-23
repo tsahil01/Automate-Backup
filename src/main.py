b98bc51da68a2b8931e3d0440c504ffc0099a1c2
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2023-08-23 19:57:40 +0530

    get file details in dictionary

--- src/main.py
+++ src/main.py
@@ -1,22 +1,39 @@
 import defaults.const as const
 import os
-import datetime
-import json
 
-
-def checkFileContent(sourceDir):
-    sourceItems = os.listdir(sourceDir)
+sourceDictionary = {}
+destinationDictionary = {}
+def checkFileContent(dir, dict):
+    sourceItems = os.listdir(dir)
     for item in sourceItems:
-        item_path = os.path.join(sourceDir, item)  # Get the full path of the item
+        item_path = os.path.join(dir, item)  # Get the full path of the item
 
         # Check if the item is a directory
-
         if os.path.isdir(item_path):
-            checkFileContent(item_path)
-
+            checkFileContent(item_path, dict)
         else:
-            print(f"{item_path}  :  {os.path.getmtime(item_path)}")
+            dict[item_path] = os.path.getmtime(item_path)
+
+
+sourceFile =  checkFileContent(dir = const.SOURCE_PATH, dict = sourceDictionary)
+destinationFile =  checkFileContent(dir = const.DESTINATION_PATH, dict = destinationDictionary)
 
 
 
-checkFileContent(sourceDir = const.SOURCE_PATH)
+
+
+def reduceDict(dict, name): 
+    rd = {}
+    for item in dict.items():
+        path = item[0]
+        modifiedTime = item[1]
+        pathAfterSource = (path.split(f'{name}'))[1]
+        rd[pathAfterSource] = modifiedTime
+    return rd
+
+
+reducedSourceDict =  reduceDict(dict= sourceDictionary, name = 'Source')
+reducedDestDict = reduceDict(dict= destinationDictionary, name = 'Destination')
+
+
+#TODO wap to check if pathAfterSource with same modifiedTime is persent on Destination or not. If not send the file or replace it in the Destination
